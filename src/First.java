f99c6307a21451a63bd562da5033bec8c8e741d4
Sabuj <msksagor@gmail.com>
2018-02-24 20:38:33 +0600

    just Welcome

---
+++ src/First.java
@@ -0,0 +1,11 @@
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to LEAD's");
+		System.out.println("Welcome to LEAD's");	System.out.println("Welcome to LEAD's");	System.out.println("Welcome to LEAD's");	System.out.println("Welcome to LEAD's");
+		
+
+	}
+
+}
