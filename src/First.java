5ffde854bc0abd9b9958d40992109fc386ec22c5
sanabeal <sanabealkabir@gmail.com>
2018-02-24 20:43:41 +0600

    add 2 numbers

--- src/First.java
+++ src/First.java
@@ -3,9 +3,11 @@ public class First {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to LEAD's");
-		System.out.println("Welcome to LEAD's");	System.out.println("Welcome to LEAD's");	System.out.println("Welcome to LEAD's");	System.out.println("Welcome to LEAD's");
+		int a = 5;
+		int b = 2;
+		int sum = a +b ;
+		System.out.println("Result is : "+sum);
 		
-
 	}
 
 }
